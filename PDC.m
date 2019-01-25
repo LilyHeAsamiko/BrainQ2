c11d9d7318176911f28ed327c5d69a66cdb2fcbb
Qin He(Lily) <45560662+LilyHeAsamiko@users.noreply.github.com>
2019-01-25 12:14:28 +0200

    commit
    https://tuni-my.sharepoint.com/:f:/r/personal/qin_he_tuni_fi/Documents/BrainQ/app%20new?csf=1&e=GJubyZ
    Find more data

--- PDC.m
+++ PDC.m
@@ -1,11 +1,19 @@
-function [j, PDCX] = PDC(X, f)
-    A = corr(X);
-    Af = fft(A);
-    j = min(abs(Af-f),1);
-    a = Af(:,j(1,2));
-    denom = sqrt(abs(a).^2); 
-    PDCX = Af./denom;
-    figure,
-    spectrogram(PDCX(:))
-    title([{'the frequency '},{int2str(f)},{' Hz is about at time '},{int2str(j(1,1))},{' ms with PDC approximation'}])
+function [PDC,A] = PDC(X,i,j,f)
+
+Xf = fft(X);
+[r,c] = find(min(abs(Xf-f)));
+for m = 1:size(X,1)
+    for n = 1:size(X,2)
+        Af(m,n) = corr(X(m,n),r(j));
+    end
+end
+
+for j = 1:size(X,2)
+    for i = 1:j
+    PDCX{i,j} = sum(Af(:,i:j),2)./sqrt((abs(Af(:,j))).^2);
+    end
+end
+
+area(PDCX{i,j});
+
 end
